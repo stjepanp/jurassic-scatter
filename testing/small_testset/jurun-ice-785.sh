79ff7b3fccbe7a0c05d76c7b53eef5189b8c28e4
Stjepan Pozgaj <stjepan.pozgaj1@gmail.com>
2021-08-29 23:32:41 +0200

    Delete unnecessary testing files

--- testing/small_testset/jurun-ice-785.sh
+++ testing/small_testset/jurun-ice-785.sh
@@ -1,22 +1,14 @@
 #!/bin/bash -x
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=4
 #SBATCH --output=out
 #SBATCH --error=err
 #SBATCH --time=1:00:00
 #SBATCH --partition=batch
 #SBATCH --account=slmet
 
-
-### load modules
-# module purge
-#module load Intel/2019.0.117-GCC-7.3.0 #GCC compiler
-#module load ParaStationMPI/5.2.1-1
-#module load GSL/2.5
-#module load netCDF/4.6.1
-
-
+module load Python/3.8.5
 module load OpenMPI/4.1.0rc1
 module load CUDA/11.3
 module load Nsight-Systems/2021.1.1
@@ -29,7 +21,7 @@ module load GCCcore/.9.3.0
 module load Intel/2020.2.254-GCC-9.3.0
 module load GSL/2.6 
 
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=4
 
 n=$(< aux/submission_index)
 m=$(( n + 1 ))
@@ -42,4 +34,4 @@ w1=785
 w2=798
 
 ### run forward model
-srun $src/formod cloud-${w1}-${w2}.ctl obs33.tab atm.tab submissions/rad-${m}.tab AEROFILE aero.tab DIRLIST aux/first_001
+srun $src/formod cloud-${w1}-${w2}.ctl obs33.tab atm.tab submissions/rad-${m}.tab AEROFILE aero.tab DIRLIST aux/first_003
